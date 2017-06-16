7b6beaf11f7c2e9ccc90b269d3a3fd0bef6c777d
galang pham <phamgalang@gmail.com>
2017-06-16 10:54:50 -0700

    dded clinet

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,22 +1,30 @@
-// @flow
-
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ReactDOM from 'react-dom'
 
-const renderApp = () => {
-  render(
-    <BrowserRouter key={Math.random()}>
-      <App />
-    </BrowserRouter>,
-    document.getElementById('app')
+const ce = React.createElement;
+
+const MyTitle = function (props) {
+  return (
+    ce('div', {id:'my-first-component'},
+      ce('h1',{style: {color: props.color}}, props.title)
+    )
   );
 };
-renderApp();
 
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    renderApp();
-  });
-}
+const MyFirstComponent = function () {
+  return (
+    ce('div', {id:'my-first-component'},
+    ce(MyTitle, {color: 'rebeccapurple', title: 'House of Cards'}),
+ce(MyTitle, {color: 'peru', title: 'Orange is the New Black'}),
+ce(MyTitle, {color: 'burlywood', title: 'Stranger Things'})
+    )
+  );
+};
+
+ReactDOM.render(
+  ce(MyFirstComponent),
+  document.getElementById("app")
+);
