0a1e90298ac41620537ce2f90483ba35f97bd3f6
galang pham <phamgalang@gmail.com>
2017-10-07 19:59:31 -0700

    added flow types

--- js/Search.jsx
+++ js/Search.jsx
@@ -8,7 +8,9 @@ class Search extends Component {
   state = {
     searchTerm: '',
   };
-  handleSearchTermChange = event => {
+  handleSearchTermChange = (
+    event: SyntheticKeyboardEvent & { target: HTMLInputElement },
+  ) => {
     this.setState({ searchTerm: event.target.value });
     // lets react know youre updating with set state
   };
